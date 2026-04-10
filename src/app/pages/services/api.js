46286700858286fb44d5f22c8b6692b4ed65b223
Rudra Prasad Mallik <rudraprasadmallik46@gmail.com>
2026-04-10 21:25:10 +0530

    new changes for live app

--- src/app/pages/services/api.js
+++ src/app/pages/services/api.js
@@ -1,7 +1,8 @@
 // Central API Service
-const BASE_URL = 'http://localhost:8080/api';
-// 'https://placement-backend-1-9jfq.onrender.com';
-// 
+const BASE_URL = 'https://placement-backend-1-9jfq.onrender.com';
+//'http://localhost:8080/api';
+
+ 
 
 
 // API endpoints configuration
